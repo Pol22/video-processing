41884183a6d24507430f9d23e5ebe920431dffbf
Pavel <pashazor22@gmail.com>
2017-02-17 13:48:44 +0300

    Create traectory points and noise points

--- video-processing/main.cpp
+++ video-processing/main.cpp
@@ -1,101 +1,66 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
-#include <chrono>
+
 
 using namespace cv;
 using namespace std;
 
-char* video_file = "D:/cv/data/768x576.avi";
 
 int main(int argc, const char** argv)
 {
-	
-	VideoCapture video(video_file);
+	srand(time(NULL));
+	int number_of_targets = 30;
+	int number_of_frames = 20;
+	int width = 1280;
+	int height = 720;
+	Mat frame(height, width, CV_8UC3, Scalar(255, 255, 255));
 
-	if (!video.isOpened())
-		return -1;
-
-	int number_of_frames = video.get(CV_CAP_PROP_FRAME_COUNT);
-
-	Mat edges;
-	// fps
-	auto start = chrono::high_resolution_clock::now();
-	auto end = chrono::high_resolution_clock::now();
-	double seconds;
-	int fps;
-	// number of points
-	int flow_num = 11;
-
-	Mat frame;
-	Mat frame_gray;
-	video >> frame;
-	cvtColor(frame, frame_gray, COLOR_RGB2GRAY);
-
-	int height = frame.cols;
-	int width = frame.rows;
-	vector<Point> initial_points;
-	map<Point, uchar> intesivity_of_points;
-
-	for (int i = height / (flow_num + 1); i < height - flow_num + 1; i += height / (flow_num + 1))
+	vector<Point> points;
+	for (int k = 0; k < number_of_targets; k++)
 	{
-		for (int j = width / (flow_num + 1); j < width - flow_num + 1; j += width / (flow_num + 1))
-		{
-			initial_points.push_back(Point(i, j));
-		}
+		points.emplace_back(rand() % width, rand() % height);
 	}
 	
-	int size_of_window = 7;
+	int number_of_noise_points = 300;
 
-	Mat previous_frame;
-	previous_frame = frame_gray;
-
-	for (int i = 1; i < number_of_frames; i++)
+	for (int p = 0; p < number_of_noise_points; p++)
 	{
-		video >> frame;
-		cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+		Point noise_point(rand() % width, rand() % height);
+		putText(frame, "+", noise_point, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+	}
 
-		for (int k = 0; k < flow_num * flow_num; k++)
+	for (int i = 0; i < number_of_frames; i++)
+	{
+		
+		for (int j = 0; j < number_of_targets; j++)
 		{
-			// search points on next frame
-			uchar previous_color = previous_frame.at<uchar>(initial_points[k]);
-			Point start_point(initial_points[k].x - size_of_window / 2, initial_points[k].y - size_of_window / 2);
-			Point next_point;
-			uchar min_differece = 255;
-			for (int p = 0; p < size_of_window; p++)
-			{
-				for (int q = 0; q < size_of_window; q++)
-				{
-					if (start_point.y + p < 0 || start_point.y + p > height ||
-						start_point.x + q < 0 || start_point.x + q > width)
-						continue;
-					uchar new_color = frame_gray.at<uchar>(start_point.y + p, start_point.x + q);
-					if (abs(new_color - previous_color) < min_differece)
-					{
-						next_point.x = start_point.x + q;
-						next_point.y = start_point.y + p;
-						min_differece = abs(new_color - previous_color);
-					}
-				}
-			}
-			line(frame, initial_points[k], next_point, Scalar(0, 0, 255), 5);
-			initial_points[k] = next_point;
-			//circle(frame, initial_points[k], 2, Scalar(0, 255, 0), 2);
+			//circle(frame, points[j], 2, Scalar(0, 255, 0), 2);
+			Point text_point(points[j].x - 13, points[j].y + 9);
+			putText(frame, "+", text_point, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+			int length_of_step = rand() % 30 + 1;
+			int angle = rand() % 360;
+			int next_x = points[j].x + length_of_step * cos(angle);
+			int next_y = points[j].y + length_of_step * sin(angle);
+
+			if (next_x < 0)
+				next_x = -next_x;
+			if (next_x > width)
+				next_x = width - (next_x - width);
+			if (next_y < 0)
+				next_y = -next_y;
+			if (next_y > height)
+				next_y = height - (next_y - height);
+
+			Point next_point(next_x, next_y);
+			text_point.x = next_x - 13;
+			text_point.y = next_y + 9;
+			putText(frame, "+", text_point, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+			line(frame, points[j], next_point, Scalar(0, 0, 255), 1);
+			points[j] = next_point;
 		}
-		previous_frame = frame_gray;
 
-		// fps
-		end = chrono::high_resolution_clock::now();
-		seconds = chrono::duration_cast<chrono::nanoseconds>(end - start).count() / double(1000000000);
-		fps = int(1 / seconds);
-		string fps_text("fps: ");
-		fps_text.append(to_string(fps));
-		putText(frame, fps_text, Point(1, 15), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(0, 255, 0), 1);
-
-
-
-		imshow("edges", frame);
-		waitKey(5);
-		start = end;
+		imshow("frame", frame);
+		waitKey(1000);
 	}
 
 	cvDestroyAllWindows();
