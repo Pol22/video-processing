d82e952cf9f3a9154cd5e3548f55320f111dfa06
Pavel <pashazor22@gmail.com>
2017-03-17 16:23:13 +0300

    Fix model and scan scope

--- video-processing/main.cpp
+++ video-processing/main.cpp
@@ -9,20 +9,19 @@
 using namespace cv;
 using namespace std;
 
-// rand from -1 to 1
+// rand from 0 to 1
 double Random()
 {
 	return double(rand()) / RAND_MAX;
 }
 
 // params model
-double vmax = 30.0;
+double vmax = 20.0;
 double r0 = 5.0;
-double changeProb = 0.1;
+double changeProb = 0.2;
 
 list<Point> trajectory;
-list<Point> predict_points;
-list<Point> previous_points;
+//list<Point> predict_points;
 list<deque<bool>> tag;
 list<bool> followed_trajectory;
 int M = 4;
@@ -38,19 +37,20 @@ void get_good_points(vector<Point> input_points, vector<Point> &output_points)
 	auto iter_traject = trajectory.begin();
 	auto iter_tag = tag.begin();
 	auto iter_follow = followed_trajectory.begin();
-	auto iter_prev = previous_points.begin();
-	auto iter_predict = predict_points.begin();
+	//auto iter_predict = predict_points.begin();
 
 	for ( ; iter_traject != trajectory.end(); )
 	{
 		Point nearest_point;
-		double min_distance = 30 + 1;
+		//double min_distance = vmax + 11; // with prediction
+		double min_distance = vmax + 1; // without prediction
 		int index_in_input = 0;
 
 		for (int j = 0; j < input_points.size(); j++)
 		{
 			
-			double dist = norm(input_points[j] - *iter_predict);
+			//double dist = norm(input_points[j] - *iter_predict); // with prediction
+			double dist = norm(input_points[j] - *iter_traject); // without prediction
 			if (dist < min_distance && employment_indexes.find(j) == employment_indexes.end())
 			{
 				min_distance = dist;
@@ -59,23 +59,24 @@ void get_good_points(vector<Point> input_points, vector<Point> &output_points)
 			}
 		}
 
-		if (min_distance > 30)
+		//if (min_distance > vmax + 10) // with prediction
+		if (min_distance > vmax) //without prediction
 		{
 			iter_tag->push_back(false);
 		}
 		else
 		{
 			iter_tag->push_back(true);
+			/*
 			// prediction
-			*iter_prev = *iter_traject;
-			*iter_traject = nearest_point;
-			double dist_predict= norm(*iter_prev - *iter_traject);
-			double koef_line = double(iter_traject->y - iter_prev->y) / (iter_traject->x - iter_prev->x);
+			double dist_predict= norm(nearest_point - *iter_traject);
+			double koef_line = double(nearest_point.y - iter_traject->y) / (nearest_point.x - iter_traject->x);
 			double alpha = atan(koef_line);
 
-			iter_predict->x = iter_traject->x + dist_predict / 2 * cos(alpha);
-			iter_predict->y = iter_traject->y + dist_predict / 2 * sin(alpha);
-
+			iter_predict->x = nearest_point.x + dist_predict / 2 * cos(alpha);
+			iter_predict->y = nearest_point.y + dist_predict / 2 * sin(alpha);
+			*/
+			*iter_traject = nearest_point;
 			employment_indexes.insert(index_in_input);
 		}
 
@@ -101,16 +102,14 @@ void get_good_points(vector<Point> input_points, vector<Point> &output_points)
 				iter_follow = followed_trajectory.erase(iter_follow);
 				iter_traject = trajectory.erase(iter_traject);
 				iter_tag = tag.erase(iter_tag); 
-				iter_prev = previous_points.erase(iter_prev);
-				iter_predict = predict_points.erase(iter_predict);
+				//iter_predict = predict_points.erase(iter_predict);
 			}
 			else
 			{
 				iter_traject++;
 				iter_tag++;
 				iter_follow++;
-				iter_prev++;
-				iter_predict++;
+				//iter_predict++;
 			}
 		}
 		else
@@ -118,8 +117,7 @@ void get_good_points(vector<Point> input_points, vector<Point> &output_points)
 			iter_traject++;
 			iter_tag++;
 			iter_follow++;
-			iter_prev++;
-			iter_predict++;
+			//iter_predict++;
 		}
 	}
 
@@ -131,8 +129,7 @@ void get_good_points(vector<Point> input_points, vector<Point> &output_points)
 		if (finded == employment_indexes.end())
 		{
 			trajectory.push_back(input_points[p]);
-			previous_points.push_back(input_points[p]);
-			predict_points.push_back(input_points[p]);
+			//predict_points.push_back(input_points[p]);
 
 			deque<bool> _tag;
 			_tag.push_back(true);
@@ -152,7 +149,7 @@ int main(int argc, const char** argv)
 	int height = 720;
 	Mat frame(height, width, CV_8UC3, Scalar(255, 255, 255));
 	// error
-	string errors_file("errors1.txt"); // error file
+	string errors_file("errors.txt"); // error file
 	ofstream err;
 	err.open(errors_file);
 	
@@ -221,15 +218,16 @@ int main(int argc, const char** argv)
             {
                 double r = r0 * Random();
                 double fi = 2 * M_PI * Random();
-                vx[j] = vx[j] + r * cos(fi);
-                vy[j] = vy[j] + r * sin(fi);
-
+				vx[j] = vx[j] + r * cos(fi);
+				vy[j] = vy[j] + r * sin(fi);
+				
                 double vv = sqrt(vx[j]*vx[j] + vy[j]*vy[j]);
                 if(vv > vmax)
                 {
                     vx[j] = vx[j] / vv * vmax;
                     vy[j] = vy[j] / vv * vmax;
                 }
+				
             }
 
 			if (next_x < 0)
@@ -290,7 +288,7 @@ int main(int argc, const char** argv)
 		err << " " << double(num_noise_points_out) / number_of_noise_points << endl;
 
 
-        char key = waitKey(40);
+        char key = waitKey(1);
 		if (key == 27)
 		{
 			break;
